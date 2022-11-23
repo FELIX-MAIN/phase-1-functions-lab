b5c41408056ba1f19c587170b1202cde2254a037
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-23 23:11:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    console.log('distance in blocks is' + distance);
+}
+distanceFromHqInBlocks (' 43')
+
+function equal(block){
+    console.log('Is equal to' + block);
+} 
+equal(' 1')
+//distance in blocks end
+
+function distanceFromHqInFeet(distance){
+    console.log('distance in feet is' + distance);
+}
+distanceFromHqInFeet(' 43')
+
+function equal(feet){
+    console.log('is equal to' + feet);
+}
+equal(' 264')
+//distance in feet
+
+function distanceTravelledInFeet(distance){
+    console.log('distance travelled in feet is' + distance);
+}
+distanceTravelledInFeet(' 43 & 48')
+
+function equal(feet){
+    console.log('is equal to' + feet);
+}
+equal(' 1320')
+//distance travelled in feet
+
+function calculatesFarePrice(destination){
+    if(destination = 400){
+        console.log('free sample' + destination);
+    }
+    else {
+        console.log('Charges apply depending on feet');
+    }
+}
+calculatesFarePrice()
